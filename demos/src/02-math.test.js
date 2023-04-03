12db73e5123029cd968e6f1eea0a0c634700089d
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-03 20:40:02 +0200

    add first tests

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,27 @@
+const {sum, multiply ,divide} = require('./02-math');
+
+test('add 1 + 3, should be 4', () => {
+  const rta = sum(1,3);
+    expect(rta).toBe(4);
+});
+
+test('should multiply ', () => {
+    const rta = multiply(1,4);
+      expect(rta).toBe(4);
+      const rta2 = multiply(5,2);
+      expect(rta2).toBe(10);
+  });
+
+  test('should divide', () => {
+    const rta = divide(12,4);
+      expect(rta).toBe(3);
+      const rta2 = divide(6,3);
+      expect(rta2).toBe(2);
+  });
+
+  test('should divide for zero', () => {
+    const rta = divide(6,0);
+      expect(rta).toBeNull();
+      const rta2 = divide(4,0);
+      expect(rta2).toBeNull();
+  });
