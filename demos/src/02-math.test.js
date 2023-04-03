ef94231b7605f6d59ee562ac6cab9a6c250cab50
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-03 21:42:14 +0200

    add setup and teardown

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,21 +1,21 @@
-const {sum, multiply ,divide} = require('./02-math');
+const { sum, multiply , divide } = require('./02-math');
 
-test('add 1 + 3, should be 4', () => {
-  const rta = sum(1,3);
+test ('add 1 + 3, should be 4', () => {
+  const rta = sum(1, 3);
     expect(rta).toBe(4);
 });
 
 test('should multiply ', () => {
-    const rta = multiply(1,4);
+    const rta = multiply(1, 4);
       expect(rta).toBe(4);
-      const rta2 = multiply(5,2);
+      const rta2 = multiply(5, 2);
       expect(rta2).toBe(10);
   });
 
   test('should divide', () => {
-    const rta = divide(12,4);
+    const rta = divide(12, 4);
       expect(rta).toBe(3);
-      const rta2 = divide(6,3);
+      const rta2 = divide(6, 3);
       expect(rta2).toBe(2);
   });
 
