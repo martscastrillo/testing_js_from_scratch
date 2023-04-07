e803f07902e28546f9a696446a6dc76b89035b7d
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-07 12:24:10 +0200

    add mockign

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,19 +1,33 @@
-const BooksService = require('./books.service')
+const BooksService = require('./books.service');
 
-describe('Test for Bookservice', ()=>{
+const fakeBooks = [
+    {
+      _id: 1,
+      name: 'Harry Potter',
+    },
+  ];
+
+const MongoLibStub = {
+    getAll: () => [...fakeBooks],
+    create: () => [],
+}; 
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(()=> MongoLibStub));
+
+describe('Test for Bookservice', () => {
     let service;
-    beforeEach(() => {
+    beforeEach (() => {
         service = new BooksService();
+        jest.ClearAllMocks();
     });
 });
 
-describe('test for getBooks', () =>{
- test('shoukd return a lis book', async ()=>{
+describe('test for getBooks', () => {
+ test('shoukd return a lis book', async () => {
        // Given
     // When
     const books = await service.getBooks({});
     console.log(books);
     // Then
-    expect(books.lenth).toEqual(2);
+    expect(books.lenth).toEqual(1);
  });
 });
