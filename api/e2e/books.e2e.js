2b79df2fbce8dec8949faab4380119e6c6f15d04
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-08 14:18:13 +0200

    add e2e tests

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,38 +1,59 @@
-const request = require ('supertest');
-
+const request = require('supertest');
+const { MongoClient } = require('mongodb');
 const createApp = require('../src/app');
-const { generateManyBook } = require('../src/fakes/book.fake');
 
-const spyGetAll = jest.fn();
+const { config } = require('../src/config');
+
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
+
+/* const { generateManyBook } = require('../src/fakes/book.fake');
+ */
+
+
+
 
 const MongoLibStub = {
-    getAll: spyGetAll,
+    getAll: jest.fn(),
     create: () => [],
 };
 
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(()=> MongoLibStub));
-
-describe('Test for books',() =>{
-    beforeAll(()=>{
+/* jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+ */
+describe('Test for books', () => {
+    let app = null;
+    let server = null;
+    let database = null;
+    beforeAll(async () => {
         app = createApp();
         server = app.listen(3001);
-     });
-     afterAll(() =>{
-          server.close();
-     });
-     describe('test for [GET] /api/v1/books', () => {
-        test('should return a list books', () =>  {
+        const client = new MongoClient(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        await client.connect();
+        database = client.db(DB_NAME);
+    });
+    afterAll(() => {
+       await server.close();
+       database.dropDatabase();
+    });
+    describe('test for [GET] /api/v1/books', () => {
+        test('should return a list books', async () => {
             // Arrange
             const fakeBooks = generateManyBook(3);
-    spyGetAll.mockResolvedValue(fakeBooks);
+            spyGetAll.mockResolvedValue(fakeBooks);
+            const seedData = await database.collection('books').insertMany([{ name: 'Book1', year: 1998, author: 'Marta' }, { name: 'Book2', year: 1998, author: 'Marta' }])
+            console.log(seedData);
+            // Act
             return request(app)
-        .get('/api/v1/books')
-        .expect(200)
-        .then(({body})=>{
-            console.log(body);
-            // Assert
-            expect(body.length).toEqual(fakeBooks.length);
-        })
+                .get('/api/v1/books')
+                .expect(200)
+                .then(({ body }) => {
+                    console.log(body);
+                    // Assert
+                    expect(body.length).toEqual(seedData.insertedCount);
+                })
         });
-      });
+    });
 });
